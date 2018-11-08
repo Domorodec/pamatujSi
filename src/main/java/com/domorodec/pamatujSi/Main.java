92bb04a2e446f4eb1eb89c5772a81209d636a86d
Domorodec <martin.fabian@centrum.cz>
2018-11-08 15:14:04 +0100

    upgrade projektu

--- src/main/java/com/domorodec/pamatujSi/Main.java
+++ src/main/java/com/domorodec/pamatujSi/Main.java
@@ -10,10 +10,40 @@ public class Main {
         System.out.println("\n");
 
         rd.readFile();
+
+        System.out.println("Pokud jste pripraveni, stisknete enter.");
+
+        try
+        {
+            System.in.read();
+        }
+        catch(Exception e)
+        {
+            System.out.println(e);
+        }
+
+        for(int i = 0; i < 10; i++){
+            System.out.println("* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * ");
+            for(int j = 0; j < 1; j++){
+                System.out.println(" * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *");
+            }
+        }
+
+
+
+
         System.out.println("Zacnete psat slova, ktera jste si zapamatovali:");
+
         CompareIt.porovnej();
         System.out.println("***************************");
         System.out.println("Puvodni hodnty byly:");
         CompareIt.tiskPole();
+
+        System.out.println("***************************************************************");
+        System.out.println("**************Nashledanou priste*******************************");
+        System.out.println("*******************Verze 1.0.***********************************");
+        System.out.println("***************************************************************");
+
     }
+
 }
