b095883361ee639f1b15a43e7fd50e5a629f7b14
Domorodec <martin.fabian@centrum.cz>
2018-11-08 11:33:16 +0100

    uprava uvodniho textu

--- src/main/java/com/domorodec/pamatujSi/Main.java
+++ src/main/java/com/domorodec/pamatujSi/Main.java
@@ -1,13 +1,15 @@
 package com.domorodec.pamatujSi;
-import java.util.Scanner;
-
 public class Main {
 
     public static void main(String[] args) {
         Read rd = new Read();
-        rd.readFile();
-        System.out.println("**************************");
+        System.out.println("***************************************************************");
+        System.out.println("**************Vitejte v programu na pamatovani slov************");
+        System.out.println("*******************Verze 1.0.***********************************");
+        System.out.println("***************************************************************");
+        System.out.println("/n");
 
+        rd.readFile();
         System.out.println("Zacnete psat slova, ktera jste si zapamatovali:");
         CompareIt.porovnej();
         System.out.println("***************************");
